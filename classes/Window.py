e361ba1458b843614d5233674a2ff62a3f721464
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-05-11 11:30:59 +0200

    added default buttons for application

--- classes/Window.py
+++ classes/Window.py
@@ -10,9 +10,32 @@ class Window:
 
         self.frame = Frame(self.root)
         self.frame.pack(padx=10, pady=10)
+        self.state = "Start"
+        
+        self.Add_buttons([
+            {'text': self.state, 'command': self.BootOrClose},
+            {'text': 'close app', 'command': self.close}
+        ])
 
     def run(self):
         self.root.mainloop()
 
     def close(self):
         self.root.destroy()
+
+    def Add_buttons(self, buttons: list):
+        for button in buttons:
+            btn = Button(self.frame, text=button['text'], command=button['command'])
+            btn.pack(pady=5)
+    def remove_buttons(self):
+        for widget in self.frame.winfo_children():
+            if isinstance(widget, Button):
+                widget.destroy()
+        
+    def BootOrClose(self):
+        self.state = "Boot" if self.state == "Start" else "Start"
+        self.remove_buttons()
+        self.Add_buttons([
+            {'text': self.state, 'command': self.BootOrClose},
+            {'text': 'close app', 'command': self.close}
+        ])
